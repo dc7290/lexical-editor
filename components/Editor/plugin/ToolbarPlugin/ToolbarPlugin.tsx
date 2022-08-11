1dfc67f04f30beae8afe143111dac8e219abea9e
dc7290 <dhkh.cba0927@gmail.com>
2022-08-11 23:38:26 +0900

    文字色、背景色を変更できるように機能追加

--- components/Editor/plugin/ToolbarPlugin/ToolbarPlugin.tsx
+++ components/Editor/plugin/ToolbarPlugin/ToolbarPlugin.tsx
@@ -1,6 +1,12 @@
+import { Menu, Transition } from '@headlessui/react'
 import { CodeIcon } from '@heroicons/react/outline'
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
+import {
+  $getSelectionStyleValueForProperty,
+  $patchStyleText,
+} from '@lexical/selection'
 import { mergeRegister } from '@lexical/utils'
+import clsx from 'clsx'
 import {
   $getSelection,
   $isRangeSelection,
@@ -8,14 +14,42 @@ import {
   FORMAT_TEXT_COMMAND,
   SELECTION_CHANGE_COMMAND,
 } from 'lexical'
-import { FC, useCallback, useEffect, useState } from 'react'
+import { FC, Fragment, useCallback, useEffect, useState } from 'react'
 
+import {
+  DEFAULT_BG_COLOR,
+  DEFAULT_TEXT_COLOR,
+} from '../../../../utils/constants'
 import { Group } from './Group'
 import { ToggleCommandButton } from './ToggleCommandButton'
+
+const colors = [
+  '#94a3b8',
+  '#f87171',
+  '#fb923c',
+  '#fbbf24',
+  '#facc15',
+  '#a3e635',
+  '#4ade80',
+  '#34d399',
+  '#2dd4bf',
+  '#22d3ee',
+  '#38bdf8',
+  '#60a5fa',
+  '#818cf8',
+  '#a78bfa',
+  '#c084fc',
+  '#e879f9',
+  '#f472b6',
+  '#fb7185',
+]
 
 const ToolbarPlugin: FC = () => {
   const [editor] = useLexicalComposerContext()
   const [activeEditor, setActiveEditor] = useState(editor)
+
+  const [textColor, setTextColor] = useState(DEFAULT_TEXT_COLOR)
+  const [bgColor, setBgColor] = useState(DEFAULT_BG_COLOR)
 
   const [isBold, setIsBold] = useState(false)
   const [isItalic, setIsItalic] = useState(false)
@@ -25,9 +59,25 @@ const ToolbarPlugin: FC = () => {
   const [isSubscript, setIsSubscript] = useState(false)
   const [isSuperscript, setIsSuperscript] = useState(false)
 
+  // 複数のuseEffectで使うためuseCallbackでメモ化した関数を定義
   const updateToolbar = useCallback(() => {
     const selection = $getSelection()
     if ($isRangeSelection(selection)) {
+      setTextColor(
+        $getSelectionStyleValueForProperty(
+          selection,
+          'color',
+          DEFAULT_TEXT_COLOR
+        )
+      )
+      setBgColor(
+        $getSelectionStyleValueForProperty(
+          selection,
+          'background-color',
+          DEFAULT_BG_COLOR
+        )
+      )
+
       setIsBold(selection.hasFormat('bold'))
       setIsItalic(selection.hasFormat('italic'))
       setIsUnderline(selection.hasFormat('underline'))
@@ -61,7 +111,7 @@ const ToolbarPlugin: FC = () => {
   }, [activeEditor, updateToolbar])
 
   return (
-    <div className="absolute top-0 flex h-12 w-full max-w-full items-center divide-x divide-gray-300 overflow-x-auto border-b border-gray-400/50 shadow-sm">
+    <div className="absolute top-0 flex h-12 w-full max-w-full flex-wrap items-center divide-x divide-gray-300  border-b border-gray-400/50 bg-indigo-50 shadow-sm">
       <Group>
         <ToggleCommandButton
           isActive={isBold}
@@ -107,6 +157,111 @@ const ToolbarPlugin: FC = () => {
         >
           <CodeIcon className="w-4" />
         </ToggleCommandButton>
+      </Group>
+
+      <Group>
+        <Menu as="div" className="relative">
+          <Menu.Button className="my-auto flex h-8 w-8 flex-col items-center justify-center font-bold leading-tight">
+            A
+            <span
+              className="h-0.5 w-3 rounded"
+              style={{ backgroundColor: textColor }}
+            ></span>
+          </Menu.Button>
+          <Transition
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+            as={Fragment}
+          >
+            <Menu.Items className="absolute -bottom-1 left-1/2 z-10 w-44 -translate-x-1/2 translate-y-full bg-white p-4 shadow-[0_0_16px_rgba(0,0,0,0.2)]">
+              <div className="grid grid-cols-6 gap-2">
+                {colors.map((color) => (
+                  <Menu.Item key={color}>
+                    {({ active }) => (
+                      <button
+                        className={clsx(
+                          (active || color === textColor) &&
+                            'ring-2 ring-indigo-700/80 ring-offset-1',
+                          'rounded pt-[100%]'
+                        )}
+                        style={{ backgroundColor: color }}
+                        onClick={() => {
+                          activeEditor.update(() => {
+                            const selection = $getSelection()
+                            if ($isRangeSelection(selection)) {
+                              $patchStyleText(selection, { color })
+                            }
+                          })
+                        }}
+                      >
+                        <span className="sr-only">
+                          Select the {`'${color}'`}
+                        </span>
+                      </button>
+                    )}
+                  </Menu.Item>
+                ))}
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+
+        <Menu as="div" className="relative">
+          <Menu.Button className="my-auto flex h-8 w-8 items-center justify-center font-bold leading-tight">
+            <span
+              className="flex h-6 w-6 items-center justify-center"
+              style={{ backgroundColor: bgColor }}
+            >
+              A
+            </span>
+          </Menu.Button>
+          <Transition
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+            as={Fragment}
+          >
+            <Menu.Items className="absolute -bottom-1 left-1/2 z-10 w-44 -translate-x-1/2 translate-y-full bg-white p-4 shadow-[0_0_16px_rgba(0,0,0,0.2)]">
+              <div className="grid grid-cols-6 gap-2">
+                {colors.map((color) => (
+                  <Menu.Item key={color}>
+                    {({ active }) => (
+                      <button
+                        className={clsx(
+                          (active || color === bgColor) &&
+                            'ring-2 ring-indigo-700/80 ring-offset-1',
+                          'rounded pt-[100%]'
+                        )}
+                        style={{ backgroundColor: color }}
+                        onClick={() => {
+                          activeEditor.update(() => {
+                            const selection = $getSelection()
+                            if ($isRangeSelection(selection)) {
+                              $patchStyleText(selection, {
+                                'background-color': color,
+                              })
+                            }
+                          })
+                        }}
+                      >
+                        <span className="sr-only">
+                          Select the {`'${color}'`}
+                        </span>
+                      </button>
+                    )}
+                  </Menu.Item>
+                ))}
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
       </Group>
 
       <Group>
---
+++ utils/constants.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_TEXT_COLOR = '#000'
+export const DEFAULT_BG_COLOR = '#fff'
